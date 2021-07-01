45aca043e9c9f636933d9139a0bf8f641fc09d8f
mirkone1994 <mirkochiappori@gmail.com>
2021-07-01 14:52:51 +0200

    calc

--- js/script.js
+++ js/script.js
@@ -5,6 +5,14 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va stampato in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.*/
 var km = prompt("Quanti km vuoi percorrere?");
-var età = prompt("Quanti nni hai?")
+var età = prompt("Quanti anni hai?")
 var prezzo = km * 0.21;
 console.log(prezzo)
+if (età <= 18) {
+    var minori = (prezzo / 20) * 100;
+    console.log(minori)
+}
+if (età >= 65) {
+    var over = (prezzo / 30) * 100;
+    console.log(over)
+}
